8856920800c8b5ee21c1b81c7e0d6ce6f0eed90f
jaybz <jaybz.e@gmail.com>
2021-01-03 03:56:51 +0800

    Revert back to default MemberSerialization to reduce clutter

--- TouchPortalApi/Models/StateCreate.cs
+++ TouchPortalApi/Models/StateCreate.cs
@@ -2,16 +2,12 @@
 
 namespace TouchPortalApi.Models
 {
-    [JsonObject(MemberSerialization.OptIn)]
     public class StateCreate
     {
         [JsonProperty]
         public static readonly string Type = "createState";
-        [JsonProperty]
         public string Id { get; set; }
-        [JsonProperty]
         public string Desc { get; set; }
-        [JsonProperty]
         public string DefaultValue { get; set; }
     }
 }
